9d422891427a19c3402aef7737a656409c47be9b
Vlad Ivanov <johndoe@example.com>
2019-02-17 14:23:05 +0300

    events improvements

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,18 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # group = Group.find(params[:group_id])
-    # @events = group.events
+    @current_date = Date.today
+
+    @param_date = params[:date].present? ? Date.parse(params[:date]) : @current_date
+    @day = params[:day].present? ? Date.parse(params[:day]) : @current_date
+    @group = Group.find(params[:group])
+    start_at = @param_date.beginning_of_month.beginning_of_week(:monday)
+    end_at = @param_date.end_of_month.end_of_week(:monday)
+    keys = (start_at..end_at).map { |date| date }
+    @events = keys.each_with_object({}) do |date, memo|
+      memo[date.strftime('%y-%m-%d')] = {
+        events: Event.where(group: @group).where('DATE(start_date) = ?', date.strftime('%d-%m-%y'))
+      }
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def same_date(date1, date2)
+    Date.parse(date1.to_s) == Date.parse(date2.to_s)
+  end
 end
