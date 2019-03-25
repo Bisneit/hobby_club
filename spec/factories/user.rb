f27411709beb31b5d4316ba3c77e303c367c0024
Bisneit <boomkinz714@gmail.com>
2019-03-25 22:25:05 +0300

    fix factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:first_name) { |n| "first_name #{n}" }
     sequence(:last_name) { |n| "first_name #{n}" }
     sequence(:email) { |n| "email#{n}@example.com" }
+    password 'password'
   end
 end
